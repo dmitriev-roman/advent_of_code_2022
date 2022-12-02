61419d0db604224d32d083143f281fb9faf7a5d5
Roman Dmitriev <roman.dmitriev@best4ad.com>
2022-12-02 12:48:55 +0400

    first task solved

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::{fs::OpenOptions, io::{BufReader, BufRead}};
+
+fn main() -> anyhow::Result<()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .open("task_data/1.txt")?;
+    let reader = BufReader::new(file);
+
+    let mut sums = vec![];
+    let mut current_sum = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+        if line == "" {
+            sums.push(current_sum);
+            current_sum = 0;
+        } else {
+            let value = line.parse::<u64>()?;
+            current_sum += value;
+        }
+    }
+    sums.push(current_sum);
+
+    sums.sort();
+    sums.reverse();
+    println!("Top sum: {}", sums.get(0).unwrap_or(&0));
+    println!("Top 3 sum: {}", sums.iter().take(3).sum::<u64>());
+    Ok(())
+}
